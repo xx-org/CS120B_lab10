e89fabab4a646da3e07a58c351c9c8a5fabfcfb6
Jia Xie <jxie031@wch136-16.cs.ucr.edu>
2019-07-11 14:07:25 -0700

    ..

--- cs120b-lab10/turnin/part3.c
+++ cs120b-lab10/turnin/part3.c
@@ -48,123 +48,14 @@ void TimerSet(unsigned long M){
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-void set_PWM(double frequency){
-	static double current_frequency;
-	if(frequency != current_frequency){
-		if(!frequency){TCCR3B &= 0x08;}
-		else{TCCR3B |=0x03;}
-		if(frequency < 0.954){OCR3A =0xFFFF;}
-		else if(frequency > 31250){OCR3A =0x0000;}
-		else{OCR3A =(short)(8000000/(128*frequency))-1;}
-		
-		TCNT3 =0;
-		current_frequency=frequency;
-	}
-}
 
-void PWM_on(){
-	TCCR3A = (1<<COM3A0);
-	TCCR3B = (1 << WGM32) | (1 << CS31)|(1<<CS30);
-	set_PWM(261.63);
-}
 
-void PWM_off(){
-	TCCR3A =0x00;
-	TCCR3B = 0x00;
-}
-const double C = 2610.63;
-const double D = 2930.66;
-const double E = 3290.63;
-
-enum states {start, wait, pressA0, pressA1, pressA2,pressing, More} state;
-unsigned int count;
-const unsigned int timer = 30;
-
-void tick(){
-	unsigned char A = ~PINA;
-	switch(state){
-		case start:
-		state = wait;
-		break;
-		case wait:
-		if( (A &0x07) == 0x01)
-		state = pressA0;
-		else if((A &0x07) ==0x02)
-		state = pressA1;
-		else if((A&0x07) == 0x04)
-		state = pressA2;
-		else if (A > 0)
-		state = More;
-		else state = wait;
-		break;
-		case pressA0:
-		if(A == 1)
-		state = pressing;
-		else state = wait;
-		break;
-		case pressA1:
-		if(A == 2)
-		state = pressing;
-		else state = wait;
-		break;
-		case pressA2:
-		if(A == 4)
-		state = pressing;
-		else state = wait;
-		break;
-		case pressing:
-		if(A != 1 && A != 0 && A != 2 && A != 4) state = More;
-		if(count>0) state = pressing;
-		else state = wait;
-		break;
-		case More:
-		if( A == 0)
-			state = wait;
-		else
-			state = More;
-		default:
-		state =start;
-		break;
-	}
-	switch(state){
-		case start:
-		break;
-		case wait:
-		count = 0;
-		PWM_off();
-		break;
-		case pressA0:
-		count = timer;
-		PWM_on();
-		set_PWM(C);
-		break;
-		case pressA1:
-		count = timer;
-		PWM_on();
-		set_PWM(D);
-		break;
-		case pressA2:
-		count = timer;
-		PWM_on();
-		set_PWM(E);
-		break;
-		case pressing:
-		if(count > 0)
-			count--;
-		else PWM_off();
-		break;
-		case More:
-		PWM_off();
-		break;
-		default:
-		break;
-	}
-}
 
 unsigned char blinkingLED = 0x00;
 enum aStates {astart,astate1, astate2, astate3} astate;
 enum bStates {bstart, bstate1, bstate2} bstate;
 enum cStates {cstart,coutput} cstate;
+enum dStates {dstart,dwait, don, doff} dstate;
 
 unsigned short acount = 0;
 const unsigned short atime = 37;
@@ -266,12 +157,74 @@ void BlinkingLEDSM(){
 	}
 }
 
+const unsigned char onoff = 2;
+unsigned char onoffcount = 0;
+void tick(){
+	switch(dstate){
+		case dstart:
+			dstate = dwait;
+			break;
+		case dwait:
+			if((~PINA & 0x04) == 0x04)
+			{	
+				dstate = don;
+				dcount = dtime;
+				onoffcount = onoff;
+			}
+			else
+				dstate = dwait;
+			break;
+		case don:
+			if(dcount == 0)
+				dstate = dwait;
+			else{
+				if(onoffcount == 0){ 
+					dstate = doff;
+					onoffcount = onoff;
+				}else{
+					onoffcount --;
+				}
+				dcount --;
+			}
+			break;
+		case doff:
+			if(dcount == 0)
+				dstate = dwait;
+			else{
+				if(onoffcount == 0){ 
+					dstate = don;
+					onoffcount = onoff;
+				}else{
+					onoffcount --;
+				}
+				dcount --;
+			}
+		default:
+			break;
+	}
+	switch(dstate){
+		case dstart:
+			break;
+		case dwait:
+			blinkingLED &= 0xEF;
+			break;
+		case don:
+			blinkingLED |= 0x10;
+			break;
+		case doff:
+			blinkingLED &= 0xEF;
+		default:
+			break;
+	}
+}
+
 void CombineLEDsSM(){
 	switch(cstate){
 		case cstart:
 			cstate = coutput;
 			break;
 		case coutput:
+			control();
 			ThreeLEDsSM();
 			BlinkingLEDSM();
 			tick();
@@ -290,14 +243,13 @@ void CombineLEDsSM(){
 			break;
 	}
 }
+
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	TimerSet(1);
 	TimerOn();
-	count = 0;
-	state = start;
     /* Insert your solution below */
     while (1) {
 		CombineLEDsSM();
